12c47d52ee2818a7120ded3904b17e88044de24d
Bindu Patil <bpatil@evoketechnologies.com>
2024-03-06 11:52:51 +0530

    added styling for header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,10 +4,11 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <div className='Header'>
+    <img src='https://api.nasa.gov/assets/footer/img/favicon-192.png' alt='logo' style={{height:'60px', position:'relative', bottom:'10px', }}/>
    <Link to='/'>All images</Link>
    <Link to='/apod'>apod</Link>
    <Link to='/neows'>neows</Link>
-   <Link to='/cmedata'>cmedata</Link> 
+   {/* <Link to='/cmedata'>cmedata</Link>  */}
     </div>
   )
 }
